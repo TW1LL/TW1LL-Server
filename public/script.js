e180050ae14fa3f1428ecde4f7058de3bb60f2f8
Will Wagner <willwagner602@gmail.com>
2016-09-30 11:02:51 -0400

    Added cookie to maintain username information for session.

--- public/script.js
+++ public/script.js
@@ -17,58 +17,77 @@ function ready() {
     userList = document.getElementById("users");
     document.getElementById("submit").addEventListener("click", sendMsg);
 
-    user.name = prompt("Please enter your name");
+    user.name = getUsername();
     socket.emit("user name", user.name);
 
     socket.on("user data", setUserData);
     socket.on("user new", addUser);
     socket.on("message receive", addMessage);
     socket.on("user list", updateUserList);
+}
 
-    function setUserData(assignedUser) {
-        user = assignedUser;
-        users[assignedUser.id] = user;
-        let el = document.getElementById(user.id) || addUser(user);
-        el.innerHTML = '<a href="#" class="user" id="'+user.id+'">' + user.name + '</a>';
-    }
-    function addUser(newUser) {
-        users[newUser.id] = newUser;
-        let li = document.createElement("li");
-        li.innerHTML = '<a href="#" class="user" id="'+newUser.id+'">' + newUser.name + '</a>';
-        li.addEventListener("click", sendTo);
-        userList.appendChild(li);
-        return li;
-    }
-    function addMessage(message) {
-        console.log(message);
-        let li = document.createElement("li");
-        let text = document.createTextNode("<" + users[message.from].name + "> " + message.text);
-        li.appendChild(text);
-        messageList.appendChild(li);
-    }
-    function sendTo(event) {
-        toBox.value = event.target.innerText;
-        toId.value = event.target.id;
-    }
-    function sendMsg() {
-        let message = {
-            "to" : toId.value,
-            "from" : user.id,
-            "text" : textBox.value,
-            "timestamp" : Date.now()
-        };
-        socket.emit("message send", message);
-        textBox.value = "";
-        addMessage(message);
-    }
+function setUserData(assignedUser) {
+    user = assignedUser;
+    users[assignedUser.id] = user;
+    let el = document.getElementById(user.id) || addUser(user);
+    el.innerHTML = '<a href="#" class="user" id="'+user.id+'">' + user.name + '</a>';
+    document.cookie = "username=" + user.name
+}
 
-    function updateUserList(userList) {
-        users = userList;
-        console.log(users);
-        userList.innerHTML = '';
-        for(var i = 0; i< userList.length; i++) {
-            addUser(userList[i]);
+function getUsername(){
+    let usernameStart = document.cookie.indexOf("username=");
+    if (usernameStart > 0){
+        let usernameEnd = document.cookie.slice(usernameStart).indexOf(";");
+        let cookieNameBuffer = 9;
+        if (usernameEnd > 0){
+            return document.cookie.slice(usernameStart + cookieNameBuffer, usernameEnd);
         }
+        return document.cookie.slice(usernameStart + cookieNameBuffer);
     }
+    else {
+        console.log(document.cookie);
+        return prompt("Please enter your name");
+    }
+}
 
+function addUser(newUser) {
+    users[newUser.id] = newUser;
+    let li = document.createElement("li");
+    li.innerHTML = '<a href="#" class="user" id="'+newUser.id+'">' + newUser.name + '</a>';
+    li.addEventListener("click", sendTo);
+    userList.appendChild(li);
+    return li;
+}
+function addMessage(message) {
+    console.log(message);
+    let li = document.createElement("li");
+    let text = document.createTextNode("<" + users[message.from].name + "> " + message.text);
+    li.appendChild(text);
+    messageList.appendChild(li);
+}
+
+function sendTo(event) {
+    toBox.value = event.target.innerText;
+    toId.value = event.target.id;
+}
+
+function sendMsg() {
+    let message = {
+        "to" : toId.value,
+        "from" : user.id,
+        "text" : textBox.value,
+        "timestamp" : Date.now()
+    };
+    socket.emit("message send", message);
+    textBox.value = "";
+    addMessage(message);
+}
+
+function updateUserList(userList) {
+    users = userList;
+    console.log(users);
+    userList.innerHTML = '';
+    for(var i = 0; i< userList.length; i++) {
+        addUser(userList[i]);
+    }
 }
